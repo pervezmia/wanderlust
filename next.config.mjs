0b28f0ec240afb546842e4b3196f9bf764d19a9e
pervezmia <ahmedpervezkabir@gmail.com>
2026-06-30 14:36:16 +0600

    next config file correction

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,9 @@ const nextConfig = {
   /* config options here */
   reactCompiler: true,
   experimental: {
-  serverExternalPackages: ['@better-auth/kysely-adapter'],
+    serverComponentsExternalPackages: ['@better-auth/kysely-adapter'],
   },
+  reactCompiler: true,
   images: {
     remotePatterns: [
       {
